f4f5e962ce5d512391a4e5ac7bd3c6163c46e59d
AjayRaja1610 <ajraja696@gmail.com>
2024-05-28 16:25:08 +0530

    ar app committed

---
+++ src/test/java/com/appRegistration/ApplicationRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
